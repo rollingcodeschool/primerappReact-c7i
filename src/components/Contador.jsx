a85901cad9a8d8c7fcdf6a5d70e7e1669381fd1d
Emilse Arias <emilsearias@gmail.com>
2022-09-12 10:54:18 -0300

    agregamos la funcion que incrementa el valor del contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"; //aqui agregamos hook
 
 const Contador = () => {
+    // creamos el state
+    //let [nombreEstado, nombre de funcion que actualiza al state ] = useState(indicar un valor inicial)
     let [counter, setCounter] = useState(9);
+    //counter = 10 esto no puedo hacer
+    //setCounter(10) asi modificamos el state
+    const sumar = ()=>{
+        console.log('desde sumar')
+        //actualizar el state
+        setCounter(counter + 1)
+    }
+
     return (
         <div>
             <h2>Contador</h2>
             <h3>{counter}</h3>
-            <button>+1</button>
+            <button onClick={sumar}>+1</button>
         </div>
     );
 };
